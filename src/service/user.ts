15e81675010417efa07b1be7013922d4da74cc5b
yeonhwa <yeonhwa6717@gmail.com>
2023-08-24 09:26:45 +0900

    feat: Add getUser service to fetch user data.
    Create a new file named 'user.tx' in the 'service' folder.
    Implement a new function called 'getUser' to retrieve the 'user.json' file and return the user JSON data.

---
+++ src/service/user.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import { promises as fs } from "fs";
+
+export type User = {
+  name: string;
+  image: string;
+  position: string;
+  introduction: string;
+  description: string;
+  contact: any;
+};
+
+export async function getUser(): Promise<User | undefined> {
+  const filePath = path.join(process.cwd(), "data", "user.json");
+  const data = await fs.readFile(filePath, "utf-8");
+  return JSON.parse(data);
+}
