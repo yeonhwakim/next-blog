b8bbe534130ee22b4426df00cbc0b673044c9c46
yeonhwa <yeonhwa6717@gmail.com>
2023-08-25 22:59:16 +0900

    style: Style profile compoennt using tailwind.

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -11,11 +11,20 @@ export default async function UserProfile() {
   const { image, name, position, introduction } = user;
 
   return (
-    <div>
-      <Image src={image} alt={name} width={200} height={200} />
-      <p>{name}</p>
-      <p>{position}</p>
-      <p>{introduction}</p>
+    <div className="w-full flex flex-col items-center justify-center">
+      <Image
+        src={image}
+        alt={name}
+        width={170}
+        height={170}
+        className="rounded-full"
+      />
+      <p className="mt-2 font-bold text-lg">{name}</p>
+      <p className="font-semibold text-md">{position}</p>
+      <p className="bg-yellow text-sm">{introduction}</p>
+      <button className="mt-2 py-1 px-3 bg-yellow-300 rounded-md text-sm font-semibold">
+        contact me!
+      </button>
     </div>
   );
 }
