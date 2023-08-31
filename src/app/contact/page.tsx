9b73a9c00b1078e4442695d1574103a1e002ea9f
yeonhwa <yeonhwa6717@gmail.com>
2023-09-01 00:10:57 +0900

    Style: style css contact page using tailwind.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -28,15 +28,20 @@ export default async function page() {
         ))}
       </ul>
       <p className="my-8 text-2xl font-bold">Or send me an email</p>
-      <div className="w-6/12 m-auto">
+      <div className="w-6/12 m-auto rounded-md bg-emerald-600 p-4">
         <form className="flex flex-col items-start" action="">
-          <label>Your Email</label>
-          <input type="email" />
-          <label>Subject</label>
-          <input type="email" />
-          <label>Message</label>
-          <input type="email" />
-          <button>submit</button>
+          <label className="font-bold text-white">Your Email</label>
+          <input className="w-full p-1 my-2 outline-0" type="email" />
+          <label className="font-bold text-white">Subject</label>
+          <input className="w-full p-1 my-2 outline-0" type="text" />
+          <label className="font-bold text-white">Message</label>
+          <textarea
+            className="w-full p-1 my-2 outline-0 resize-none"
+            rows={5}
+          ></textarea>
+          <button className="w-full mt-2 rounded-md bg-yellow-300 py-2 font-semibold text-lg">
+            submit
+          </button>
         </form>
       </div>
     </div>
