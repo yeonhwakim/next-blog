8e7352ca2622d8c24bdff4efdb8f5df07eb5ee6a
yeonhwa <yeonhwa6717@gmail.com>
2023-09-05 23:11:25 +0900

    feat: Add apis related post.
    Create a new file named posts.ts to fetch posts.
    Define the getPosts function to retrieve all posts.
    Define the getPostsByCategory function to retrive posts by a catogory argument.
    Define the getPostsByFeatured function to retrice posts by an isFeatured argument.

---
+++ src/service/posts.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import { promises as fs } from "fs";
+
+export type Post = {
+  title: string;
+  description: string;
+  date: string;
+  category: string;
+  path: string;
+  featured: boolean;
+};
+
+export async function getPosts(): Promise<Post[]> {
+  const filePath = path.join(process.cwd(), "data", "posts.json");
+  const data = await fs.readFile(filePath, "utf-8");
+  return JSON.parse(data);
+}
+
+export async function getPostsByCategory(category: string): Promise<Post[]> {
+  const posts = await getPosts();
+  const postsByCategory = posts.filter((post) => post.category === category);
+  return postsByCategory;
+}
+
+export async function getPostsByFeatured(isFeatured: boolean): Promise<Post[]> {
+  const posts = await getPosts();
+  const postsByFeatured = posts.filter((post) => post.featured === isFeatured);
+  return postsByFeatured;
+}
