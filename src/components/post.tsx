592a1113b60acd98a18934fbf4ca6853c4bae453
yeonhwa <yeonhwa6717@gmail.com>
2023-09-12 23:13:52 +0900

    refactor: Refactor post component markup using semantic tags.

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -16,24 +16,24 @@ export default async function PostItem({ post }: PostPsops) {
   return (
     <>
       <li className="w-65 drop-shadow-md">
-        <Link href={`/posts/${path}`}>
-          <Image
-            src={`/images/posts/${path}.png`}
-            alt={title}
-            width={500}
-            height={500}
-          />
-          <div className="p-2">
-            <p className="text-right mb-2 text-xs">{date}</p>
-            <div className="text-center text-sm">
-              <p className="font-bold text-sm">{title}</p>
+        <article>
+          <Link href={`/posts/${path}`}>
+            <Image
+              src={`/images/posts/${path}.png`}
+              alt={title}
+              width={500}
+              height={500}
+            />
+            <div className="p-2">
+              <time className="text-right mb-2 text-xs">{date}</time>
+              <h3 className="font-bold text-sm">{title}</h3>
               <p className="text-sm my-1">{description}</p>
               <span className="text-xs px-3 py-1 rounded-full bg-yellow-200">
                 {category}
               </span>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </article>
       </li>
     </>
   );
