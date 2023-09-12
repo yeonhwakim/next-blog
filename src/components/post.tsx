4d7abc3a3f26183704be4e42aea13c8c2c256bb0
yeonhwa <yeonhwa6717@gmail.com>
2023-09-12 22:46:25 +0900

    feat: Router post detail page using Link component.

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,5 +1,6 @@
 import { Post } from "@/service/posts";
 import Image from "next/image";
+import Link from "next/link";
 
 type PostPsops = {
   posts: Post[];
@@ -10,22 +11,24 @@ export default async function PostItem({ posts }: PostPsops) {
     <>
       {posts.map(({ title, description, date, category, path }) => (
         <li key={title} className="w-65 drop-shadow-md">
-          <Image
-            src={`/images/posts/${path}.png`}
-            alt={title}
-            width={500}
-            height={500}
-          />
-          <div className="p-2">
-            <p className="text-right mb-2 text-xs">{date}</p>
-            <div className="text-center text-sm">
-              <p className="font-bold text-sm">{title}</p>
-              <p className="text-sm my-1">{description}</p>
-              <span className="text-xs px-3 py-1 rounded-full bg-yellow-200">
-                {category}
-              </span>
+          <Link href={`/posts/${path}`}>
+            <Image
+              src={`/images/posts/${path}.png`}
+              alt={title}
+              width={500}
+              height={500}
+            />
+            <div className="p-2">
+              <p className="text-right mb-2 text-xs">{date}</p>
+              <div className="text-center text-sm">
+                <p className="font-bold text-sm">{title}</p>
+                <p className="text-sm my-1">{description}</p>
+                <span className="text-xs px-3 py-1 rounded-full bg-yellow-200">
+                  {category}
+                </span>
+              </div>
             </div>
-          </div>
+          </Link>
         </li>
       ))}
     </>
