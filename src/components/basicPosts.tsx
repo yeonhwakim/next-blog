500b28d7a3fa82684cfdc436b56d9a190ae93bf8
yeonhwa <yeonhwa6717@gmail.com>
2023-09-14 22:38:18 +0900

    fix: Resolve error.

--- src/components/basicPosts.tsx
+++ src/components/basicPosts.tsx
@@ -5,7 +5,7 @@ type PostsProps = {
   posts: Post[];
 };
 
-export default async function BasicPosts({ posts }: PostsProps) {
+export default function BasicPosts({ posts }: PostsProps) {
   return (
     <>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
