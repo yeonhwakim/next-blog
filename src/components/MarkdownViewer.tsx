fecf5a1ae39cb172384cba768b17b03927a976f8
yeonhwa <yeonhwa6717@gmail.com>
2023-09-15 23:52:25 +0900

    feat: Implement react-symtax-highlighter.

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,5 +1,9 @@
+"use client";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
 
 type MarkdownViewerProps = {
   content: string;
@@ -7,7 +11,37 @@ type MarkdownViewerProps = {
 
 export default function MarkdownViewer({ content }: MarkdownViewerProps) {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className="prose max-w-none"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              style={materialDark}
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            src={image.src || ""}
+            alt={image.alt || ""}
+            width={500}
+            height={350}
+          />
+        ),
+      }}
+    >
       {content}
     </ReactMarkdown>
   );
