21e720ebae2ce3458e24be9fcea9e331b7eefb0a
yeonhwa <yeonhwa6717@gmail.com>
2023-09-17 11:12:29 +0900

    refactor: Refactor Email component.

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,48 +1,70 @@
 "use client";
 
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+type Email = {
+  from: string;
+  subject: string;
+  message: string;
+};
 
 export default function Email() {
-  const initialInputValues: { [key: string]: string } = {
-    email: "",
+  const [form, setForm] = useState<Email>({
+    from: "",
     subject: "",
     message: "",
+  });
+
+  const { from, subject, message } = form;
+
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { value, name } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
   };
 
-  const [inputValues, setInputValues] = useState(initialInputValues);
-  const { email, subject, message } = inputValues;
-
-  const onChangeInput = (event: any) => {
-    const { value, name: inputName } = event.target;
-    setInputValues({ ...inputValues, [inputName]: value });
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
   };
-
   return (
     <div className="w-6/12 m-auto rounded-md bg-emerald-600 p-4">
-      <form className="flex flex-col items-start">
-        <label className="font-bold text-white">Your Email</label>
+      <form className="flex flex-col items-start" onSubmit={onSubmit}>
+        <label htmlFor="from" className="font-bold text-white">
+          Your Email
+        </label>
         <input
           className="w-full p-1 my-2 outline-0"
           type="email"
-          name="email"
-          value={email}
-          onChange={onChangeInput}
+          name="from"
+          id="from"
+          required
+          autoFocus
+          value={from}
+          onChange={onChange}
         />
-        <label className="font-bold text-white">Subject</label>
+        <label htmlFor="subject" className="font-bold text-white">
+          Subject
+        </label>
         <input
           className="w-full p-1 my-2 outline-0"
           type="text"
           name="subject"
+          id="subject"
+          required
           value={subject}
-          onChange={onChangeInput}
+          onChange={onChange}
         />
-        <label className="font-bold text-white">Message</label>
+        <label htmlFor="message" className="font-bold text-white">
+          Message
+        </label>
         <textarea
           className="w-full p-1 my-2 outline-0 resize-none"
           rows={5}
           name="message"
+          id="message"
+          required
           value={message}
-          onChange={onChangeInput}
+          onChange={onChange}
         ></textarea>
         <button
           className="w-full mt-2 rounded-md bg-yellow-300 py-2 font-semibold text-lg"
