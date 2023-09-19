c3e25b86ed4b5dd619458be817d75f94fd77cfe4
yeonhwa <yeonhwa6717@gmail.com>
2023-09-19 23:42:45 +0900

    feat: Add sendMail api using nodemailer.

--- src/service/Email.ts
+++ src/service/Email.ts
@@ -1,5 +1,33 @@
+import nodemailer from "nodemailer";
+
 export type EmailData = {
   from: string;
   subject: string;
   message: string;
 };
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.AUTH_USER,
+    pass: process.env.AUTH_PASS,
+  },
+});
+
+export function sendEmail({ from, subject, message }: EmailData) {
+  const mailData = {
+    to: process.env.AUTH_USER,
+    from,
+    subject: `[BLOG] ${subject}`,
+    html: `
+      <h1>${subject}</h1>
+      <p>${message}</p>
+      <br/>
+      <p>보낸사람: ${from}</p>
+    `,
+  };
+
+  return transporter.sendMail(mailData);
+}
