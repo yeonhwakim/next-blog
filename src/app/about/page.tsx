12f7162835251c81bbcf4afabaea1c18d4ba24f6
yeonhwa <yeonhwa6717@gmail.com>
2023-09-21 23:19:38 +0900

    feat: Add About page metadata.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
 import UserProfile from "@/components/profile";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Me",
+  description: "연화의 커리어 소개",
+};
 
 export default function page() {
   return (
